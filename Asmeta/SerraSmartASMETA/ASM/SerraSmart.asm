f4dfc1d0fa088b25b8ba19c6b467c5a5169c57ae
Samuel Gherardi <samuel.gherardi@gmail.com>
2025-07-02 22:53:35 +0200

    Definizione delle regole di accensione luci in SerraSmart.asm
    Creazione delle regole r_luci , che permette l'accensione o lo spegnimento di una singola luce, e r_luciAccese che va ad accendere le luci della serra una volta che la luminosità è sotto la soglia minima.

--- Asmeta/SerraSmartASMETA/ASM/SerraSmart.asm
+++ Asmeta/SerraSmartASMETA/ASM/SerraSmart.asm
@@ -1,87 +1,98 @@
-// Nome asm
 asm SerraSmart
 
-// Importazione delle librerie necessarie (librerie standard ASMETA + logiche CTL e LTL per model checking)
 import ../STDL/StandardLibrary
 import ../STDL/CTLLibrary
 import ../STDL/LTLLibrary
 
 signature:
+	// DOMINI
+	// Domini rappresentanti gli elementi della SerraSmart e lo stato degli elementi
+	domain Luci subsetof Integer
+	domain Irrigatori subsetof Integer
+	domain LivelloIrrigatore subsetof Integer
+	domain Temperatura subsetof Integer
+	domain Umidita subsetof Integer
+	domain Luminosita subsetof Integer
+	enum domain Ventilatori = {PRINCIPALE | SECONDARIO}
+	enum domain StatoVentilatore = {ACCESO | SPENTO}
+	enum domain StatoLuce = {ON | OFF}
+	
+	// Domini per le funzioni monitorate (input dall'environment)
+	enum domain Elementi = {SERRA | LUCI | IRRIGATORI | VENTILATORI /* | ANTIFURTO*/}
+	enum domain AzioniSerra = {CHIUDI_TUTTO | APRI_TUTTO}
+	enum domain AzioniLuci = {ACCENDI_LUCE | SPEGNI_LUCE}
+	enum domain AzioniIrrigatori = {IMPOSTA_IRRIGATORE | APRI_IRRIGATORE | CHIUDI_IRRIGATORE}
+	enum domain AzioniVentilatori = {ACCENDI_VENTILATORE | SPEGNI_VENTILATORE}
+	
+	// FUNZIONI
+	controlled statoLuce: Luci -> StatoLuce
+	controlled statoIrrigatore: Irrigatori -> LivelloIrrigatore
+	controlled statoVentilatore: Ventilatori -> StatoVentilatore
 
-// ===============================
-// DEFINIZIONE DEI DOMINI
-// ===============================
-// Domini enumerativi
-enum domain StatoAttuatore = {ON | OFF}
-enum domain ModalitaControllo = {AUTOMATICO | MANUALE}
+	monitored elemento: Elementi
+	monitored azioneSerra: AzioniSerra
+	monitored azioneLuci: AzioniLuci
+	monitored azioneIrrigatori: AzioniIrrigatori
+	monitored azioneVentilatori: AzioniVentilatori
+	monitored luce: Luci
+	monitored irrigatore: Irrigatori
+	monitored ventilatore: Ventilatori
+	monitored livello_irrigatore: LivelloIrrigatore
+	monitored sogliaTempMin : Integer
+	monitored sogliaTempMax : Integer
+	monitored sogliaUmiditaMin : Integer
+	monitored sogliaUmiditaMax : Integer
+	monitored sogliaLuceMin : Integer
 
-// Tipi di attuatori
-enum domain Attuatore = {VENTOLA | IRRIGAZIONE | LUCE}
-
-// ==============================
-// Domini astratti
-abstract domain Utente
-
-// ===============================
-// FUNZIONI MONITORATE (input esterni)
-// ===============================
-monitored temperatura : Integer
-monitored umidita : Integer
-monitored luce : Integer
-monitored modalita : ModalitaControllo
-
-// ===============================
-// FUNZIONI CONTROLLATE (attuatori)
-// ===============================
-controlled statoAttuatore : Attuatore -> StatoAttuatore
-
-// ===============================
-// FUNZIONI STATICHE (soglie configurabili)
-// ===============================
-static sogliaTempMin : Integer
-static sogliaTempMax : Integer
-static sogliaUmiditaMin : Integer
-static sogliaUmiditaMax : Integer
-static sogliaLuceMin : Integer
-static sogliaLuceMax : Integer
-
-// ===============================
-// FUNZIONI DERIVATE
-// ===============================
-
-// Controllo delle soglie della temperatura
-derived temperaturaTroppoBassa : Boolean
-derived temperaturaTroppoAlta : Boolean
-
-// Controllo soglie umidità
-derived umiditaTroppoBassa : Boolean
-derived umiditaTroppoAlta : Boolean
-
-// Controllo soglie luce
-derived luceTroppoBassa : Boolean
-derived luceTroppoAlta : Boolean
-
+	derived serraChiusa: Boolean //True se la serra ha luci spente, irrigatori spenti e ventilatori spenti
+	
 definitions:
-// ===============================
-// DEFINIZIONE FUNZIONI DERIVATE
-// ===============================
+	// DEFINIZIONE DOMINI
+	domain Luci = {1 : 5}
+	domain Irrigatori = {1 : 3}
+	domain LivelloIrrigatore = {0 : 100} //0% = completamente chiuso; 100% = completamente aperto
+	domain Temperatura = {1:100} //per modellare la temperatura rilevata
+	domain Umidita = {1:100} //per modellare l'umidità rilevata
+	domain Luminosita = {1:100} //per modellare la luminosità rilevata
+	
+	// DEFINIZIONE FUNZIONI	
+	function serraChiusa = 	(forall $l in Luci with statoLuce($l) = OFF) and
+							(forall $i in Irrigatori with statoIrrigatore($i) = 0) and
+							(forall $v in Ventilatori with statoVentilatore($v) = SPENTO)
+	
+	// DEFINIZIONE DELLE REGOLE
+	// Regola che va ad accendere oppure a spegnere una singola luce della serra
+	rule r_luci($azione in AzioniLuci) = 
+		if ($azione = ACCENDI_LUCE)	
+		then statoLuce(luce) := ON
+		else 	if ($azione = SPEGNI_LUCE)
+				then statoLuce(luce) := OFF
+				endif
+		endif
+	
+	// Regola che va ad accendere tutte le luci nel momento in cui la luminosità segnalata è minore della soglia minima
+	rule r_luciAccese($azione in AzioniLuci) = 
+		forall $l in Luci with statoLuce($l) = OFF
+		do
+			statoLuce($l) := ON						
 
-// Controllo soglie temperatura
-function temperaturaTroppoBassa = if(temperatura < sogliaTempMin) then true else false endif
-function temperaturaTroppoAlta = if(temperatura > sogliaTempMax) then true else false endif
-
-
-// Controllo soglie umidità
-function umiditaTroppoBassa = if(umidita < sogliaUmiditaMin) then true else false endif
-function umiditaTroppoAlta = if(umidita > sogliaUmiditaMax) then true else false endif
-
-// Controllo soglie luce
-function luceTroppoBassa = if(luce < sogliaLuceMin) then true else false endif
-function luceTroppoAlta = if(luce > sogliaLuceMax) then true else false endif
-
+	
+	// REGOLA PRINCIPALE
+	main rule r_Main =
+		// Si simula in modo casuale il livello di luminosità presente nella serra
+		choose $x in Luminosita with true
+		do 	if ($x < sogliaLuceMin)
+			then r_luciAccese[azioneLuci]
+			endif				
+	
+	
 
 
 
+// STATO INIZIALE
+default init s0:
+	// Luci spente
+	function statoLuce($l in Luci) =  OFF
 
 
 
